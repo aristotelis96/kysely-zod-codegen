faecec2de4cd891f6407c5f6e087895c9749c04b
Aristotelis Vontzalidis <aristotelis@nolimitcity.com>
2024-11-06 11:15:46 +0100

    Selectable date as string for postgres

--- src/generator/dialects/postgres/postgres-adapter.ts
+++ src/generator/dialects/postgres/postgres-adapter.ts
@@ -81,7 +81,10 @@ export class PostgresAdapter extends Adapter {
       new PropertyNode('y', new IdentifierNode('number')),
     ]),
     Timestamp: new ColumnTypeNode(
-      new IdentifierNode('Date'),
+      new UnionExpressionNode([
+        new IdentifierNode('Date'),
+        new IdentifierNode('string'),
+      ]),
       new UnionExpressionNode([
         new IdentifierNode('Date'),
         new IdentifierNode('string'),
